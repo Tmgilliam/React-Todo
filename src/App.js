e2298a7f2be5edbfd1a7f43b8dd4dccfbc16466d
Tatianna <gilliamtatiannam@gmail.com>
2019-06-12 16:17:18 -0700

    app and todoList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import TodoList from "./components/TodoComponents/TodoList";
 
 class App extends React.Component {
+
+  state = {
+    todoList: [
+      {
+        task: 'Call Oprah',
+        id: 1528817077286,
+        completed: false
+      },
+      {
+        task: 'Fofty owes me money',
+        id: 1528817084358,
+        completed: false
+      }
+    ],
+
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className='container'>
+          <h2>My Todo List</h2>
+
+          <TodoList todoList = {this.state.todoList}/>
+        </div>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import "./Todo.css";
+
+
+
+const TodoList = (props) =>{
+    console.log(props)
+    console.log(props.todoList)
+
+     return(
+         <div>
+             {props.todoList.map(todo => (
+                 <div key ={todo.id}>{todo.task}</div>
+             ))}
+         </div>
+     )
+
+}
+
+export default TodoList;
